0fa76fe626269fd71fba02ad9267dbc1b8cf78f5
Александр Попов <alex-popov1506@yandex.ru>
2023-11-21 22:35:42 +0400

    красивый вывод готов

--- CharacterSheetBuilder.py
+++ CharacterSheetBuilder.py
@@ -5,28 +5,31 @@ from Character import Character
 
 
 class CharacterSheetBuilder:
-    def __init__(self, user_request):
+    def __init__(self, user_request, user_setting):
         self.user_request = user_request
+        self.user_setting = user_setting
         self.llm = ChatOpenAI(openai_api_key=os.environ['API_KEY_OPENAI'])
-        self.character_sheet = Character(
-            description=self.get_description()
-
-        )
+        # self.character_sheet = Character(
+        #     description=self.get_description()
+        #
+        # )
 
     def get_description(self):
-        art_promt = f'Напиши краткое художественное описание для несуществующего персонажа' \
-                          f' используя данный запрос {self.user_request}'
-        description = self.llm.invoke(art_promt)
+        art_promt = f'Напиши краткое художественное описание внешности и одежды для персонажа' \
+                    f'существующего в рамках сеттинга описываемого как {self.user_setting}' \
+                    f' используя данное краткое описание: {self.user_request}'
+        description = self.llm.invoke(art_promt).content
+
         return description
 
     # def get_ability_stats(self):
-    #     pass
+    #     stats_promt = f''
 
 
 load_dotenv()
 
-user_request = input()
+user_request = input('Введите краткое описание персонажа: ')
+user_setting = input('Введите краткое описание сеттинга: ')
 
-
-char_build = CharacterSheetBuilder(user_request)
-print(char_build.character_sheet)
+char_build = CharacterSheetBuilder(user_request, user_setting)
+print(char_build.get_description())
